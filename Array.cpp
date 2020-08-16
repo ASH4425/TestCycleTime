966aca7d627730cbfa7670c75d0542a72c8ac427
소현 안 <thgus4425@naver.com>
2020-08-17 01:46:21 +0900

    View

--- Array.cpp
+++ Array.cpp
@@ -143,8 +143,8 @@ double Array::ReadCell(int x, int y, char* mode) {
 				//if (x == 1 && y == 1) cout << conductanceLocal << endl;
 				//if(x==1 && y == 1) cout << static_cast<eNVM*>(cell[x][y])->conductance << endl;
 
-				cellCurrent = readVoltage / (1/ (static_cast<eNVM*>(cell[x][y])->conductance * (pow((timeZero/ static_cast<eNVM*>(cell[x][y])->waitTime),0.031))) + totalWireResistance);
-				
+				cellCurrent = readVoltage / (1/ (static_cast<eNVM*>(cell[x][y])->conductance) + totalWireResistance);
+				//cellCurrent = readVoltage / (1 / (static_cast<eNVM*>(cell[x][y])->conductance * (pow((timeZero / static_cast<eNVM*>(cell[x][y])->waitTime), 0.031))) + totalWireResistance);
 			}
 		}
         //printf("The current is %.4e\n",cellCurrent);
