be2b46d0793e41937a0e7536dc19fa05e5b95749
소현 안 <thgus4425@naver.com>
2020-08-17 03:04:52 +0900

    View

--- Train.cpp
+++ Train.cpp
@@ -188,7 +188,7 @@ void Train(const int numTrain, const int epochs, char* optimization_type) {
 								static_cast<eNVM*>(arrayIH->cell[j][k])->waitTime = cycleWaitTimeIH[j][k];
 
 								double timeRatio = (1e-06) / static_cast<eNVM*>(arrayIH->cell[j][k])->waitTime;
-								static_cast<eNVM*>(arrayIH->cell[j][k])->conductance *= pow(timeRatio, 0.031);
+								//static_cast<eNVM*>(arrayIH->cell[j][k])->conductance *= pow(timeRatio, 0.031);
 
 								//static_cast<eNVM*>(arrayIH->cell[j][k])->conductance *= pow((1e-06/(cycleWaitTimeIH[j][k])), 0.031);
 
