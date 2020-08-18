4967a86229765fbe0acbba039940d59b30500db5
소현 안 <thgus4425@naver.com>
2020-08-18 13:36:16 +0900

    Add variation

--- Cell.cpp
+++ Cell.cpp
@@ -555,7 +555,7 @@ void RealDevice::DriftWrite(int x, int y, double weight, double waitTimeParamete
 	double maxResistance = 2e06;
 	*/
 
-	driftCoeffDepend = (maxdriftCoeff - mindriftCoeff) / (log(maxResistance) - log(minResistance));
+	driftCoeffDepend = (maxdriftCoeff - mindriftCoeff) / (log(maxResistance/minResistance));
 
 	if (x == 4 && y == 2) {
 		cout << maxdriftCoeff << '\n';
