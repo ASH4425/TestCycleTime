37bcf3c86d65c3d7e5176be09e0520efbee0252f
소현 안 <thgus4425@naver.com>
2020-08-18 21:23:17 +0900

    WithDriftNoVarRe

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withDriftNoVar.csv");                                                                                                            
+	mywriteoutfile.open("withDriftNoVarRe.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
