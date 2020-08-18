c13b4fd84d1b844df7b0a0a518ec99fcd7166cc0
소현 안 <thgus4425@naver.com>
2020-08-18 21:11:58 +0900

    WithDriftNoVar

--- Cell.cpp
+++ Cell.cpp
@@ -582,13 +582,13 @@ void RealDevice::DriftWrite(int x, int y, double weight, double waitTimeParamete
 	if (driftCoeff < mindriftCoeff) driftCoeff = mindriftCoeff;
 	if (driftCoeff > maxdriftCoeff) driftCoeff = maxdriftCoeff;
 
-	/*
+	
 	if (x == 60 && y == 60) {
 		cout << "conductance at DriftWrite() before : " << conductance << '\n';
-		cout << "WaitTime : " << waitTimeParameter << '\n';
+		//cout << "WaitTime : " << waitTimeParameter << '\n';
 		cout << "driftCoeff : " << driftCoeff << '\n';
 	}
-	*/
+	
 	/*
 	if (x == 1 && y == 1) {
 		cout << conductance << '\n';
@@ -596,7 +596,7 @@ void RealDevice::DriftWrite(int x, int y, double weight, double waitTimeParamete
 	}
 	*/
 
-	conductance *= pow((1e-03 / waitTimeParameter), driftCoeff);
+	conductance *= pow((1e-03 / 0.1), driftCoeff);
 
 	/*
 	if (x == 1 && y == 1) {
@@ -629,12 +629,11 @@ void RealDevice::DriftWrite(int x, int y, double weight, double waitTimeParamete
 		conductance = minConductance;
 	}
 	
-	/*
+	
 	if (x == 60 && y == 60) {
 		cout << "conductance at DriftWrite() after : " << conductance << '\n';
-		cout << "WaitTime : " << waitTimeParameter << '\n';
 	}
-	*/
+	
 	
 	
 }
