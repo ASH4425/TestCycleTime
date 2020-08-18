f1fb5593ccd2940c4cf1d6acd2f84b13f4376bfd
소현 안 <thgus4425@naver.com>
2020-08-19 00:10:43 +0900

    D2D 10

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withDriftNoVarRe.csv");                                                                                                            
+	mywriteoutfile.open("withDriftD2D10.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
