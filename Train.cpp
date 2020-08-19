cfb8e6bf8f4037bc77f531bfaa1674ed42d0ff15
소현 안 <thgus4425@naver.com>
2020-08-19 09:34:50 +0900

    D2D20

--- Train.cpp
+++ Train.cpp
@@ -1208,7 +1208,7 @@ void Train(const int numTrain, const int epochs, char* optimization_type) {
 				for (int m = 0; m < param->nHide; m++) {
 					for (int i = 0; i < 4; i++) {
 						for (int n = 100 * i; n < 100 * (i + 1); n++) {
-							string filenameC = "weightIHD2D5";
+							string filenameC = "weightIHD2D20";
 							std::ofstream readC;
 							readC.open(filenameC + ".csv", std::ios_base::app);
 							readC << endl;
@@ -1221,7 +1221,7 @@ void Train(const int numTrain, const int epochs, char* optimization_type) {
 				for (int m = 0; m < param->nOutput; m++) {
 					for (int i = 0; i < 4; i++) {
 						for (int n = 25 * i; n < 25 * (i + 1); n++) {
-								string filenameD = "weightHOD2D5";
+								string filenameD = "weightHOD2D20";
 								std::ofstream readD;
 								readD.open(filenameD + ".csv", std::ios::out | std::ios_base::app);
 								readD << endl;
--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withDriftD2D5.csv");                                                                                                            
+	mywriteoutfile.open("withDriftD2D20.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
