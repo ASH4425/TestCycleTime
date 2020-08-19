e1624d4897d00e735962031df95db603e6487dd1
소현 안 <thgus4425@naver.com>
2020-08-19 14:13:37 +0900

    Test D2D 5

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withDriftD2D5Test.csv");                                                                                                            
+	mywriteoutfile.open("withDriftNoVarTest.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
