2bc2ec2340b96236fb2c42f856875dd3ac968128
소현 안 <thgus4425@naver.com>
2020-08-19 21:40:38 +0900

    C2C2

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("withDriftC2C5Ree.csv");                                                                                                            
+	mywriteoutfile.open("withDriftC2C2.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
