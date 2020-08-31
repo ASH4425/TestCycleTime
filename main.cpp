4ee28d2bb70c1b7ac13df6a571aea704d4685322
소현 안 <thgus4425@naver.com>
2020-09-01 01:17:55 +0900

    One minute

--- main.cpp
+++ main.cpp
@@ -128,7 +128,7 @@ int main() {
 	srand(0);	// Pseudorandom number seed
 	
 	ofstream mywriteoutfile;
-	mywriteoutfile.open("oneminute.csv");                                                                                                            
+	mywriteoutfile.open("oneminuteRe.csv");                                                                                                            
 	
 	for (int i=1; i<=param->totalNumEpochs/param->interNumEpochs; i++) {
 
